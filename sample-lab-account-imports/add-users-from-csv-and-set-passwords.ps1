417c31d0f97ab2c2bf64b210a0bbc0365aba6f1f
Stephen Sullivan <stsulliv@gmail.com>
2018-08-09 10:02:39 -0400

    Update add-users-from-csv-and-set-passwords.ps1

--- sample-lab-account-imports/add-users-from-csv-and-set-passwords.ps1
+++ sample-lab-account-imports/add-users-from-csv-and-set-passwords.ps1
@@ -1,3 +1,3 @@
-﻿# 
+# Edit below to match file names and location
 
-Import-Csv .\User.csv | New-ADUser -Enabled $True -CannotChangePassword $True -PasswordNeverExpires $True -AccountPassword (ConvertTo-SecureString -string S0meP@ssw0rd -AsPlainText -force) -department YourDepartment
+Import-Csv .\sample_lab_users_for_import.csv | New-ADUser -Enabled $True -CannotChangePassword $True -PasswordNeverExpires $True -AccountPassword (ConvertTo-SecureString -string S0meP@ssw0rd -AsPlainText -force)
