5a5244722dbc2c03d541dd59c7694c27ddfabbea
Stephen Sullivan <stsulliv@gmail.com>
2018-08-09 09:43:35 -0400

    Add files via upload

---
+++ sample-lab-account-imports/add-users-from-csv-and-set-passwords.ps1
@@ -0,0 +1,3 @@
+﻿# 
+
+Import-Csv .\User.csv | New-ADUser -Enabled $True -CannotChangePassword $True -PasswordNeverExpires $True -AccountPassword (ConvertTo-SecureString -string S0meP@ssw0rd -AsPlainText -force) -department YourDepartment
